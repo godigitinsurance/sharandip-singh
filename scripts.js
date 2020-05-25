63432c4f52d4cca2eb1e840d32f8503aff934e48
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-25 16:34:23 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Sharandip Singh");
+$("#policyNumber").text("OB26211085");
+$("#startDate").text("26-May-2020");
+$("#endDate").text("25-May-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08AX9037");
+$("#make").text("TVS MOTORS");
+$("#model").text("STAR CITY");
+$("#vehicalSubType").text("STD (100 CC)");
+$("#engineNo").text("00201");
+$("#chassis").text("68192");
